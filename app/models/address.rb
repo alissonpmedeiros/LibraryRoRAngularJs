5defb8b0ed68d7bb1326238fd59e45fa7fb42a5a
alissonpmedeiros <alissonp.medeiros@gmail.com>
2016-08-11 16:50:26 -0300

    add addresses controller.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ActiveRecord::Base
   belongs_to :state
   belongs_to :user
 
-  accepts_nested_attributes_for :states
-  accepts_nested_attributes_for :users
+  #accepts_nested_attributes_for :states
+  #accepts_nested_attributes_for :users
 end
