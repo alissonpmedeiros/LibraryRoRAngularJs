7b4a9d18d7761ed196e1529ad242370d51000d36
alissonpmedeiros <alissonp.medeiros@gmail.com>
2016-08-11 11:07:56 -0300

    refactoring State's controller and service

--- app/assets/javascripts/services/StatesService.js
+++ app/assets/javascripts/services/StatesService.js
@@ -1,12 +1,5 @@
 angular.module('library').factory('StatesService', ['$resource', function($resource) {
     return $resource('/states.json', {}, {
-        query:  {method: 'GET', isArray: true},
-        create: {method: 'POST'}
-    })
-}]).factory('StateService', ['$resource', function($resource) {
-    return $resource('/states/stateId.json', {}, {
-        show:   {method: 'GET'},
-        update: {method: 'PUT',    params: {stateId: '@id'}},
-        delete: {method: 'DELETE', params: {stateId: '@id'}}
+        query:  {method: 'GET', isArray: true}
     })
 }]);
