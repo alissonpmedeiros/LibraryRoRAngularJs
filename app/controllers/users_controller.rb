ef32aacac6c3cdfd882d9ae1c1f28e2434cf3f67
alissonpmedeiros <alissonp.medeiros@gmail.com>
2016-08-13 15:57:35 -0300

    generate permission to current_user update attribute :address_registrable

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render json: @user.as_json, status: :ok
+    else
+      render json: {book: @book.errors, status: :unprocessable_entity}
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:provider, :uid, :encrypted_password, :reset_password_token,
+                                 :reset_password_sent_at, :remember_created_at, :sign_in_count,
+                                 :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip,
+                                 :last_sign_in_ip, :name, :nickname, :image, :email, :tokens,
+                                 :address_registrable)
+  end
+
+
+
+end
