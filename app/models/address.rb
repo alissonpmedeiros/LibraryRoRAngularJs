4461cf87988a3d8c5b87d869e7805f16682b97a5
alissonpmedeiros <alissonp.medeiros@gmail.com>
2016-08-14 16:20:12 -0300

    refactoring loan model to adapter user admin

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
   belongs_to :state
-  belongs_to :user_session
+  belongs_to :user
 
   #accepts_nested_attributes_for :states
   #accepts_nested_attributes_for :users
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,5 @@
 class Loan < ActiveRecord::Base
+  belongs_to :admin, class_name: 'User'
   belongs_to :user
   belongs_to :book
 end
